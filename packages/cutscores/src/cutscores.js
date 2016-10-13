966a37c0a512a7146eeee5daa348d8ba3c632771
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-10-13 11:21:13 -0400

    Add trajectory support for touch devices

--- packages/cutscores/src/cutscores.js
+++ packages/cutscores/src/cutscores.js
@@ -338,22 +338,42 @@ function drawScores (selection, scores, x, y) {
     .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-  function turnOn (el) {
-    d3.select(el)
-      .attr('data-is-selected', true)
+  function displayTrajectory (d) {
+    var c = d3.select(this);
+
+    if (d3.event.type === 'wheel') d3.event.preventDefault();
+
+    d3.selectAll('.trajectory').style('stroke-opacity', 0);
+
+    var tp = +(c.attr('data-trajectory-percentile'));
+    var tpNew = tp + (d3.event.type === 'wheel' ? d3.event.deltaY : Math.round(-d3.event.dy));
+    tpNew = Math.min(99, Math.max(1, tpNew));
+    c.attr('data-trajectory-percentile', tpNew);
+    d3.select('#test' + d.level + '_trajectory_' + tpNew).style('stroke-opacity', 1);
+  }
+
+  function turnOn (el, d) {
+    var c = d3.select(el);
+
+    c.attr('data-is-selected', true)
       .style('stroke-width', 4)
       .style('cursor', 'ns-resize');
 
-    if (d3.select(el).attr('data-trajectory-percentile') === null) {
-      d3.select(el).attr('data-trajectory-percentile', 50);
-    }
+    var tp = c.attr('data-trajectory-percentile');
+    d3.select('#test' + d.level + '_trajectory_' + tp).style('stroke-opacity', 1);
+
+    c.on('wheel', displayTrajectory);
+    c.call(d3.drag().on('drag', displayTrajectory));
   }
 
   function turnOff (els) {
+    d3.selectAll('.trajectory').style('stroke-opacity', 0);
     d3.selectAll(els)
       .attr('data-is-selected', false)
       .style('stroke-width', 2)
-      .style('cursor', 'default');
+      .style('cursor', 'default')
+      .on('wheel', null)
+      .on('.drag', null);
   }
 
   svg
@@ -364,6 +384,7 @@ function drawScores (selection, scores, x, y) {
       .attr('r', 10)
       .attr('cx', d => x(d.level))
       .attr('cy', d => y(d.score))
+      .attr('data-trajectory-percentile', 50)
       .style('fill', 'white')
       .style('stroke', '#666')
       .style('stroke-width', 2)
@@ -377,32 +398,14 @@ function drawScores (selection, scores, x, y) {
       })
       .on('click', function (d, i, collection) {
         if (!d.trajectories) return;
+        var doSelect = d3.select(this).attr('data-is-selected') !== 'true';
 
-        var c = d3.select(this);
-        var isSelected = c.attr('data-is-selected') !== 'true';
-
+        // unselect all circles
         turnOff(collection);
-        d3.selectAll('.trajectory').style('stroke-opacity', 0);
 
-        if (isSelected) {
-          turnOn(this);
-          var tp = d3.select(this).attr('data-trajectory-percentile');
-          d3.select('#test' + d.level + '_trajectory_' + tp).style('stroke-opacity', 1);
+        // if the clicked circle wasn't already selected, select it
+        if (doSelect) {
+          turnOn(this, d);
         }
-      })
-      .on('wheel', function (d) {
-        var c = d3.select(this);
-
-        if (c.attr('data-is-selected') !== 'true') return;
-
-        d3.event.preventDefault();
-        d3.selectAll('.trajectory').style('stroke-opacity', 0);
-
-        var tp = +(c.attr('data-trajectory-percentile'));
-        var tpNew = tp + d3.event.deltaY;
-        tpNew = Math.min(99, Math.max(1, tpNew));
-        c.attr('data-trajectory-percentile', tpNew);
-        d3.select('#test' + d.level + '_trajectory_' + tpNew).style('stroke-opacity', 1);
       });
-
 }
