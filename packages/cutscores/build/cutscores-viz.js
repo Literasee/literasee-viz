5e668f0d0ad7ebd2f8bf28604a395dfdf632d12d
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-10-13 16:14:17 -0400

    Move score drawing into its own file

--- packages/cutscores/build/cutscores-viz.js
+++ packages/cutscores/build/cutscores-viz.js
@@ -18082,6 +18082,79 @@ var drawTrajectories = function (selection, scores, x, y, colors) {
   });
 }
 
+var drawScores = function (selection, scores, x, y) {
+  function displayTrajectory (d) {
+    var c = d3.select(this);
+
+    if (d3.event.type === 'wheel') { d3.event.preventDefault(); }
+
+    d3.selectAll('.trajectory').style('stroke-opacity', 0);
+
+    var tp = +(c.attr('data-trajectory-percentile'));
+    var tpNew = tp + (d3.event.type === 'wheel' ? d3.event.deltaY : Math.round(-d3.event.dy));
+    tpNew = Math.min(99, Math.max(1, tpNew));
+    c.attr('data-trajectory-percentile', tpNew);
+    d3.select('#test' + d.level + '_trajectory_' + tpNew).style('stroke-opacity', 1);
+  }
+
+  function turnOn (el, d) {
+    var c = d3.select(el);
+
+    c.attr('data-is-selected', true)
+      .style('stroke-width', 4)
+      .style('cursor', 'ns-resize');
+
+    var tp = c.attr('data-trajectory-percentile');
+    d3.select('#test' + d.level + '_trajectory_' + tp).style('stroke-opacity', 1);
+
+    c.on('wheel', displayTrajectory);
+    c.call(d3.drag().on('drag', displayTrajectory));
+  }
+
+  function turnOff (els) {
+    d3.selectAll('.trajectory').style('stroke-opacity', 0);
+    d3.selectAll(els)
+      .attr('data-is-selected', false)
+      .style('stroke-width', 2)
+      .style('cursor', 'default')
+      .on('wheel', null)
+      .on('.drag', null);
+  }
+
+  selection
+    .selectAll('circle')
+    .data(scores)
+    .enter()
+    .append('circle')
+      .attr('r', 10)
+      .attr('cx', function (d) { return x(d.level); })
+      .attr('cy', function (d) { return y(d.score); })
+      .attr('data-trajectory-percentile', 50)
+      .style('fill', 'white')
+      .style('stroke', '#666')
+      .style('stroke-width', 2)
+      .style('pointer-events', 'auto') // needed because parent ignores events
+      .on('mouseover mouseout', function (d) {
+        if (!d.trajectories) { return; }
+        if (d3.select(this).attr('data-is-selected') === 'true') { return; }
+
+        d3.select(this)
+          .style('cursor', d3.event.type === 'mouseover' ? 'pointer' : 'default');
+      })
+      .on('click', function (d, i, collection) {
+        if (!d.trajectories) { return; }
+        var doSelect = d3.select(this).attr('data-is-selected') !== 'true';
+
+        // unselect all circles
+        turnOff(collection);
+
+        // if the clicked circle wasn't already selected, select it
+        if (doSelect) {
+          turnOn(this, d);
+        }
+      });
+}
+
 var interp = d3.interpolateRgb('red', 'blue');
 if (window['pym']) { var pymChild = new pym.Child(); }
 
@@ -18164,8 +18237,8 @@ var cutscores = function (selector, args) {
         createSVG(layer).call(drawGrowthLines, scores, x, y, interp);
         // create a new, absolutely positioned SVG to house the trajectory lines
         createSVG(layer).call(drawTrajectories, scores, x, y, interp);
-        layer
-          .call(drawScores, scores, x, y);
+        // create a new, absolutely positioned SVG to house the score bubbles
+        createSVG(layer).call(drawScores, scores, x, y);
 
       } else {
         var cutscoreSet = stateData.pop();
@@ -18307,81 +18380,6 @@ function drawBackground (selection, data, x, y, ratio, absolute) {
 
   // next line only needed if chart will be updated
   // bands.attr('d', area);
-}
-
-function drawScores (selection, scores, x, y) {
-  var svg = createSVG(selection);
-
-  function displayTrajectory (d) {
-    var c = d3.select(this);
-
-    if (d3.event.type === 'wheel') { d3.event.preventDefault(); }
-
-    d3.selectAll('.trajectory').style('stroke-opacity', 0);
-
-    var tp = +(c.attr('data-trajectory-percentile'));
-    var tpNew = tp + (d3.event.type === 'wheel' ? d3.event.deltaY : Math.round(-d3.event.dy));
-    tpNew = Math.min(99, Math.max(1, tpNew));
-    c.attr('data-trajectory-percentile', tpNew);
-    d3.select('#test' + d.level + '_trajectory_' + tpNew).style('stroke-opacity', 1);
-  }
-
-  function turnOn (el, d) {
-    var c = d3.select(el);
-
-    c.attr('data-is-selected', true)
-      .style('stroke-width', 4)
-      .style('cursor', 'ns-resize');
-
-    var tp = c.attr('data-trajectory-percentile');
-    d3.select('#test' + d.level + '_trajectory_' + tp).style('stroke-opacity', 1);
-
-    c.on('wheel', displayTrajectory);
-    c.call(d3.drag().on('drag', displayTrajectory));
-  }
-
-  function turnOff (els) {
-    d3.selectAll('.trajectory').style('stroke-opacity', 0);
-    d3.selectAll(els)
-      .attr('data-is-selected', false)
-      .style('stroke-width', 2)
-      .style('cursor', 'default')
-      .on('wheel', null)
-      .on('.drag', null);
-  }
-
-  svg
-    .selectAll('circle')
-    .data(scores)
-    .enter()
-    .append('circle')
-      .attr('r', 10)
-      .attr('cx', function (d) { return x(d.level); })
-      .attr('cy', function (d) { return y(d.score); })
-      .attr('data-trajectory-percentile', 50)
-      .style('fill', 'white')
-      .style('stroke', '#666')
-      .style('stroke-width', 2)
-      .style('pointer-events', 'auto') // needed because parent ignores events
-      .on('mouseover mouseout', function (d) {
-        if (!d.trajectories) { return; }
-        if (d3.select(this).attr('data-is-selected') === 'true') { return; }
-
-        d3.select(this)
-          .style('cursor', d3.event.type === 'mouseover' ? 'pointer' : 'default');
-      })
-      .on('click', function (d, i, collection) {
-        if (!d.trajectories) { return; }
-        var doSelect = d3.select(this).attr('data-is-selected') !== 'true';
-
-        // unselect all circles
-        turnOff(collection);
-
-        // if the clicked circle wasn't already selected, select it
-        if (doSelect) {
-          turnOn(this, d);
-        }
-      });
 }
 
 exports.cutscores = cutscores;
--- packages/cutscores/src/cutscores.js
+++ packages/cutscores/src/cutscores.js
@@ -13,6 +13,7 @@ import addGutterCuts from './addGutterCuts';
 import createCutScales from './createCutScales';
 import drawGrowthLines from './drawGrowthLines';
 import drawTrajectories from './drawTrajectories';
+import drawScores from './drawScores';
 
 const w = 800;
 const h = 400;
@@ -85,8 +86,8 @@ export default function (selector = 'body', args) {
         createSVG(layer).call(drawGrowthLines, scores, x, y, interp);
         // create a new, absolutely positioned SVG to house the trajectory lines
         createSVG(layer).call(drawTrajectories, scores, x, y, interp);
-        layer
-          .call(drawScores, scores, x, y);
+        // create a new, absolutely positioned SVG to house the score bubbles
+        createSVG(layer).call(drawScores, scores, x, y);
 
       } else {
         const cutscoreSet = stateData.pop();
@@ -223,79 +224,4 @@ function drawBackground (selection, data, x, y, ratio = 1, absolute = true) {
 
   // next line only needed if chart will be updated
   // bands.attr('d', area);
-}
-
-function drawScores (selection, scores, x, y) {
-  var svg = createSVG(selection);
-
-  function displayTrajectory (d) {
-    var c = d3.select(this);
-
-    if (d3.event.type === 'wheel') d3.event.preventDefault();
-
-    d3.selectAll('.trajectory').style('stroke-opacity', 0);
-
-    var tp = +(c.attr('data-trajectory-percentile'));
-    var tpNew = tp + (d3.event.type === 'wheel' ? d3.event.deltaY : Math.round(-d3.event.dy));
-    tpNew = Math.min(99, Math.max(1, tpNew));
-    c.attr('data-trajectory-percentile', tpNew);
-    d3.select('#test' + d.level + '_trajectory_' + tpNew).style('stroke-opacity', 1);
-  }
-
-  function turnOn (el, d) {
-    var c = d3.select(el);
-
-    c.attr('data-is-selected', true)
-      .style('stroke-width', 4)
-      .style('cursor', 'ns-resize');
-
-    var tp = c.attr('data-trajectory-percentile');
-    d3.select('#test' + d.level + '_trajectory_' + tp).style('stroke-opacity', 1);
-
-    c.on('wheel', displayTrajectory);
-    c.call(d3.drag().on('drag', displayTrajectory));
-  }
-
-  function turnOff (els) {
-    d3.selectAll('.trajectory').style('stroke-opacity', 0);
-    d3.selectAll(els)
-      .attr('data-is-selected', false)
-      .style('stroke-width', 2)
-      .style('cursor', 'default')
-      .on('wheel', null)
-      .on('.drag', null);
-  }
-
-  svg
-    .selectAll('circle')
-    .data(scores)
-    .enter()
-    .append('circle')
-      .attr('r', 10)
-      .attr('cx', d => x(d.level))
-      .attr('cy', d => y(d.score))
-      .attr('data-trajectory-percentile', 50)
-      .style('fill', 'white')
-      .style('stroke', '#666')
-      .style('stroke-width', 2)
-      .style('pointer-events', 'auto') // needed because parent ignores events
-      .on('mouseover mouseout', function (d) {
-        if (!d.trajectories) return;
-        if (d3.select(this).attr('data-is-selected') === 'true') return;
-
-        d3.select(this)
-          .style('cursor', d3.event.type === 'mouseover' ? 'pointer' : 'default');
-      })
-      .on('click', function (d, i, collection) {
-        if (!d.trajectories) return;
-        var doSelect = d3.select(this).attr('data-is-selected') !== 'true';
-
-        // unselect all circles
-        turnOff(collection);
-
-        // if the clicked circle wasn't already selected, select it
-        if (doSelect) {
-          turnOn(this, d);
-        }
-      });
 }
