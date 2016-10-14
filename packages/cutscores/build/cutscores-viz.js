d2c318a27c5ee81e2179d2d9d651d6f5567eb14c
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-10-13 23:14:38 -0400

    Move background drawing into its own file

--- packages/cutscores/build/cutscores-viz.js
+++ packages/cutscores/build/cutscores-viz.js
@@ -18022,6 +18022,127 @@ var createCutScales = function (cuts, width, height) {
   };
 }
 
+var drawBackground = function (svg, data, x, y, height) {
+  var colors = ['#525252', '#737373', '#969696', '#BDBDBD', '#D9D9D9'];
+
+  var cut_scores = data.cuts;
+  var numLevels = data.levels.length;
+
+  // create an X axis using the original length of cut_scores as number of ticks
+  var xAxis = d3.axisBottom(x)
+    .ticks(cut_scores.length - 2)
+    .tickFormat(function (d, i) {
+      // use the test field for tick labels
+      // +1 skips the fake data point we created at the front of the array
+      var realCut = cut_scores[i+1];
+      if (realCut.year) { return ((realCut.test) + " / " + (realCut.year)); }
+      return ("" + (realCut.test));
+    })
+    .tickSizeOuter(0);
+
+  // draw X axis below chart
+  svg
+    .append('g')
+    .attr('transform', ("translate(0, " + height + ")"))
+    .call(xAxis);
+
+  // Y axes, for debugging only
+  // svg.append('g').call(d3.axisLeft(y));
+  // svg.append('g').attr('transform', `translate(${width}, 0)`).call(d3.axisRight(y));
+
+
+  // generate keys from data
+  var keys = ['hoss'];
+  for (var i = numLevels - 1; i > 0; i--) {
+    keys.unshift('cut' + i);
+  }
+
+  // generate state by calculating diffs between cuts
+  var stack = d3.stack()
+    .keys(keys)
+    .offset(function (series, order) {
+      for (var i = series.length - 1; i > -1; i--) {
+        for (var j = 0; j < series[i].length; j++) {
+          // bottom band runs from loss to cut1
+          // others run from prev cut
+          if (i === 0) {
+            series[i][j][0] = y.domain()[0];
+          } else {
+            series[i][j][0] = series[i - 1][j][1];
+          }
+
+          // top band runs from last cut to hoss to fill chart area
+          if (i === series.length - 1) { series[i][j][1] = y.domain()[1]; }
+        }
+      }
+    });
+
+  var area = d3.area()
+    .x(function (d) { return x(d.data.level); })
+    .y0(function (d) { return y(d[0]); })
+    .y1(function (d) { return y(d[1]); })
+    .curve(d3.curveCatmullRom.alpha(0.5));
+
+  var bands = svg
+    .selectAll('.layer')
+    .data(stack(cut_scores))
+    .enter()
+      .append('g')
+      .attr('class', 'layer')
+      .on('mouseover', function () {
+        d3.select(this)
+          .select('path')
+          .style('fill-opacity', 0.5)
+          .style('stroke-width', 0.8);
+
+        d3.select(this)
+          .select('text')
+          .style('fill-opacity', 1);
+      })
+      .on('mouseout', function () {
+        d3.select(this)
+          .select('path')
+          .style('fill-opacity', 0.5)
+          .style('stroke-width', .1);
+
+        d3.select(this)
+          .select('text')
+          .style('fill-opacity', 0);
+      });
+
+  // next line only needed if chart will be updated
+  // bands.exit().remove();
+
+  // chart is (currently) only rendered once, so everything happens in enter
+  bands
+    .append('path')
+    .style('fill', function (d) {
+      return d3.scaleOrdinal(colors).domain(keys)(d.key);
+    })
+    .style('fill-opacity', 0.5)
+    .style('stroke', 'black')
+    .style('stroke-width', .1)
+    .attr('d', area);
+
+  bands
+    .append('text')
+    .attr('font-size', 11)
+    .attr('font-family', 'sans-serif')
+    .attr('x', 5)
+    .attr('y', function (d, i) {
+      var datum = d[0];
+      // datum[0] is the bottom value, datum[1] the top
+      var mid = datum[0] + ((datum[1] - datum[0]) / 2);
+      return y(mid);
+    })
+    .attr('dy', '0.4em')
+    .style('fill-opacity', 0)
+    .text(function (d, i) { return data.levels[i].label; });
+
+  // next line only needed if chart will be updated
+  // bands.attr('d', area);
+}
+
 var drawGrowthLines = function (selection, scores, x, y, colors) {
   var line = d3.line()
     .x(function (d) { return x(d.level); })
@@ -18201,17 +18322,18 @@ var cutscores = function (selector, args) {
           if (ratio < 1) {
             x.range([0, w * ratio - margin.left - margin.right]);
 
-            container
+            var splitWrapper = container
               .append('div')
               .attr('id', Date.now())
               .style('width', ratio * 100 + '%')
               .style('display', 'inline-block')
               .style('position', 'absolute')
-              .style(i ? 'right' : 'left', 0)
-              .call(drawBackground, cutscoreSet, x, y, ratio);
+              .style(i ? 'right' : 'left', 0);
+
+            createSVG(splitWrapper, 'absolute', ratio)
+              .call(drawBackground, cutscoreSet, x, y, height);
           } else {
-            container
-              .call(drawBackground, cutscoreSet, x, y);
+            createSVG(container).call(drawBackground, cutscoreSet, x, y, height);
           }
         });
 
@@ -18231,7 +18353,7 @@ var cutscores = function (selector, args) {
           .append('div')
             .attr('id', Date.now())
             .style('position', 'relative')
-            .style('pointer-events', 'none')
+            .style('pointer-events', 'none');
 
         // create a new, absolutely positioned SVG to house the growth lines
         createSVG(layer).call(drawGrowthLines, scores, x, y, interp);
@@ -18246,140 +18368,12 @@ var cutscores = function (selector, args) {
         var ref$2 = createCutScales(cutscoreSet.cuts, width, height);
         var x$1 = ref$2.x;
         var y$1 = ref$2.y;
-        container.call(drawBackground, cutscoreSet, x$1, y$1, 1, false);
+        createSVG(container, 'relative').call(drawBackground, cutscoreSet, x$1, y$1, height);
       }
 
       if (pymChild) { pymChild.sendHeight(); }
 
     });
-}
-
-function drawBackground (selection, data, x, y, ratio, absolute) {
-  if ( ratio === void 0 ) ratio = 1;
-  if ( absolute === void 0 ) absolute = true;
-
-  var colors = ['#525252', '#737373', '#969696', '#BDBDBD', '#D9D9D9'];
-
-  var cut_scores = data.cuts;
-  var numLevels = data.levels.length;
-
-  // base with margins
-  var position = !absolute ? 'relative' : 'absolute';
-  var svg = createSVG(selection, position, ratio);
-
-  // create an X axis using the original length of cut_scores as number of ticks
-  var xAxis = d3.axisBottom(x)
-    .ticks(cut_scores.length - 2)
-    .tickFormat(function (d, i) {
-      // use the test field for tick labels
-      // +1 skips the fake data point we created at the front of the array
-      var realCut = cut_scores[i+1];
-      if (realCut.year) { return ((realCut.test) + " / " + (realCut.year)); }
-      return ("" + (realCut.test));
-    })
-    .tickSizeOuter(0);
-
-  // draw X axis below chart
-  svg
-    .append('g')
-    .attr('transform', ("translate(0, " + height + ")"))
-    .call(xAxis);
-
-  // Y axes, for debugging only
-  // svg.append('g').call(d3.axisLeft(y));
-  // svg.append('g').attr('transform', `translate(${width}, 0)`).call(d3.axisRight(y));
-
-
-  // generate keys from data
-  var keys = ['hoss'];
-  for (var i = numLevels - 1; i > 0; i--) {
-    keys.unshift('cut' + i);
-  }
-
-  // generate state by calculating diffs between cuts
-  var stack = d3.stack()
-    .keys(keys)
-    .offset(function (series, order) {
-      for (var i = series.length - 1; i > -1; i--) {
-        for (var j = 0; j < series[i].length; j++) {
-          // bottom band runs from loss to cut1
-          // others run from prev cut
-          if (i === 0) {
-            series[i][j][0] = y.domain()[0];
-          } else {
-            series[i][j][0] = series[i - 1][j][1];
-          }
-
-          // top band runs from last cut to hoss to fill chart area
-          if (i === series.length - 1) { series[i][j][1] = y.domain()[1]; }
-        }
-      }
-    });
-
-  var area = d3.area()
-    .x(function (d) { return x(d.data.level); })
-    .y0(function (d) { return y(d[0]); })
-    .y1(function (d) { return y(d[1]); })
-    .curve(d3.curveCatmullRom.alpha(0.5));
-
-  var bands = svg
-    .selectAll('.layer')
-    .data(stack(cut_scores))
-    .enter()
-      .append('g')
-      .attr('class', 'layer')
-      .on('mouseover', function () {
-        d3.select(this)
-          .select('path')
-          .style('fill-opacity', 0.5)
-          .style('stroke-width', 0.8);
-
-        d3.select(this)
-          .select('text')
-          .style('fill-opacity', 1);
-      })
-      .on('mouseout', function () {
-        d3.select(this)
-          .select('path')
-          .style('fill-opacity', 0.5)
-          .style('stroke-width', .1);
-
-        d3.select(this)
-          .select('text')
-          .style('fill-opacity', 0);
-      });
-
-  // next line only needed if chart will be updated
-  // bands.exit().remove();
-
-  // chart is (currently) only rendered once, so everything happens in enter
-  bands
-    .append('path')
-    .style('fill', function (d) {
-      return d3.scaleOrdinal(colors).domain(keys)(d.key);
-    })
-    .style('fill-opacity', 0.5)
-    .style('stroke', 'black')
-    .style('stroke-width', .1)
-    .attr('d', area);
-
-  bands
-    .append('text')
-    .attr('font-size', 11)
-    .attr('font-family', 'sans-serif')
-    .attr('x', 5)
-    .attr('y', function (d, i) {
-      var datum = d[0];
-      // datum[0] is the bottom value, datum[1] the top
-      var mid = datum[0] + ((datum[1] - datum[0]) / 2);
-      return y(mid);
-    })
-    .attr('dy', '0.4em')
-    .style('fill-opacity', 0)
-    .text(function (d, i) { return data.levels[i].label; });
-
-  // next line only needed if chart will be updated
-  // bands.attr('d', area);
 }
 
 exports.cutscores = cutscores;
---
+++ packages/cutscores/src/drawBackground.js
@@ -0,0 +1,120 @@
+export default function (svg, data, x, y, height) {
+  var colors = ['#525252', '#737373', '#969696', '#BDBDBD', '#D9D9D9'];
+
+  var cut_scores = data.cuts;
+  var numLevels = data.levels.length;
+
+  // create an X axis using the original length of cut_scores as number of ticks
+  var xAxis = d3.axisBottom(x)
+    .ticks(cut_scores.length - 2)
+    .tickFormat((d, i) => {
+      // use the test field for tick labels
+      // +1 skips the fake data point we created at the front of the array
+      var realCut = cut_scores[i+1];
+      if (realCut.year) return `${realCut.test} / ${realCut.year}`;
+      return `${realCut.test}`;
+    })
+    .tickSizeOuter(0);
+
+  // draw X axis below chart
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(xAxis);
+
+  // Y axes, for debugging only
+  // svg.append('g').call(d3.axisLeft(y));
+  // svg.append('g').attr('transform', `translate(${width}, 0)`).call(d3.axisRight(y));
+
+
+  // generate keys from data
+  var keys = ['hoss'];
+  for (var i = numLevels - 1; i > 0; i--) {
+    keys.unshift('cut' + i);
+  }
+
+  // generate state by calculating diffs between cuts
+  var stack = d3.stack()
+    .keys(keys)
+    .offset((series, order) => {
+      for (var i = series.length - 1; i > -1; i--) {
+        for (var j = 0; j < series[i].length; j++) {
+          // bottom band runs from loss to cut1
+          // others run from prev cut
+          if (i === 0) {
+            series[i][j][0] = y.domain()[0];
+          } else {
+            series[i][j][0] = series[i - 1][j][1];
+          }
+
+          // top band runs from last cut to hoss to fill chart area
+          if (i === series.length - 1) series[i][j][1] = y.domain()[1];
+        }
+      }
+    });
+
+  var area = d3.area()
+    .x(d => x(d.data.level))
+    .y0(d => y(d[0]))
+    .y1(d => y(d[1]))
+    .curve(d3.curveCatmullRom.alpha(0.5));
+
+  var bands = svg
+    .selectAll('.layer')
+    .data(stack(cut_scores))
+    .enter()
+      .append('g')
+      .attr('class', 'layer')
+      .on('mouseover', function () {
+        d3.select(this)
+          .select('path')
+          .style('fill-opacity', 0.5)
+          .style('stroke-width', 0.8);
+
+        d3.select(this)
+          .select('text')
+          .style('fill-opacity', 1);
+      })
+      .on('mouseout', function () {
+        d3.select(this)
+          .select('path')
+          .style('fill-opacity', 0.5)
+          .style('stroke-width', .1);
+
+        d3.select(this)
+          .select('text')
+          .style('fill-opacity', 0);
+      });
+
+  // next line only needed if chart will be updated
+  // bands.exit().remove();
+
+  // chart is (currently) only rendered once, so everything happens in enter
+  bands
+    .append('path')
+    .style('fill', (d) => {
+      return d3.scaleOrdinal(colors).domain(keys)(d.key);
+    })
+    .style('fill-opacity', 0.5)
+    .style('stroke', 'black')
+    .style('stroke-width', .1)
+    .attr('d', area);
+
+  bands
+    .append('text')
+    .attr('font-size', 11)
+    .attr('font-family', 'sans-serif')
+    .attr('x', 5)
+    .attr('y', (d, i) => {
+      var datum = d[0];
+      // datum[0] is the bottom value, datum[1] the top
+      var mid = datum[0] + ((datum[1] - datum[0]) / 2);
+      return y(mid);
+    })
+    .attr('dy', '0.4em')
+    .style('fill-opacity', 0)
+    .text((d, i) => data.levels[i].label);
+
+  // next line only needed if chart will be updated
+  // bands.attr('d', area);
+}
