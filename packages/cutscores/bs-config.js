12a8e4ae20913217fb05455c400e5f58a5f649fa
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-10-21 10:50:01 -0400

    Enable pulling from local literasee repo

--- packages/cutscores/bs-config.js
+++ packages/cutscores/bs-config.js
@@ -9,12 +9,16 @@ module.exports = {
   ghostMode: false,
   open: 'external',
   host: 'local.literasee.io',
-  port: 3000,
+  port: 3001,
   startPath: 'demo-host.html',
   rewriteRules: [
     {
       match: 'https://data.literasee.io',
       replace: 'http://localhost:4000'
+    },
+    {
+      match: 'literasee.io/public/host-utils.js',
+      replace: 'view.local.literasee.io:3000/public/host-utils.js'
     }
   ]
 };
