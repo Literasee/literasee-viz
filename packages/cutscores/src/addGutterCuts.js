010989c09c07603e241ed4df03388f28ffc7a63c
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2016-11-28 15:24:18 -0500

    Comment fix

--- packages/cutscores/src/addGutterCuts.js
+++ packages/cutscores/src/addGutterCuts.js
@@ -3,8 +3,10 @@ export default function (cuts, gutter = 0.5) {
   // right on the edges of our chart, we need to create some fake data entries
   // we will essentially find the min and max grades and create entries
   // slightly below and above them, respectively
-
   // 0 to 1, how much of a "grade" should the gutters represent?
+  // this function also adds `level` properties to each cut
+  // to assure there is a numeric field to use for ordering, etc.
+
   const n = cuts.length;
 
   return [].concat(
